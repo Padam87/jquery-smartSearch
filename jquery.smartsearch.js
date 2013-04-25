73fcc0a9e0419d5db880b2627b76b651d553ed21
Adam Prager <prager.adam87@gmail.com>
2013-04-25 12:23:58 +0200

    added a license

--- jquery.smartsearch.js
+++ jquery.smartsearch.js
@@ -1,6 +1,21 @@
-// jQuery SmartSearch
-// A kick-ass search plugin for your site
-// by Adam Prager <adam.prager@netlife.hu>
+/* ===========================================================
+* jquery.smartsearch.js
+* http://padam87.github.io/jquery-smartSearch/
+* ===========================================================
+* Copyright 2013 Adam Prager
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* =========================================================== */
 ;(function($) {
     $.smartSearch = function(el, options) {
         var defaults = {
@@ -15,7 +30,7 @@
         var smartSearch = this;
 
         smartSearch.settings = {}
-        
+
         smartSearch.initialized = false;
 
         var init = function() {
@@ -24,34 +39,34 @@
             smartSearch.$search = $(el).find('.search').bind('focus', function() {
                 smartSearch.$filters.show();
             });
-            
+
             smartSearch.$form.find("select, input").change(smartSearch.settings.onChange);
 
             smartSearch.createSearchbar();
             smartSearch.createFilters();
-            
+
             smartSearch.initialized = true;
         }
-        
+
         /* Clears filters */
         smartSearch.clearFilters = function() {
             smartSearch.$searchbar.find('li:not(.handle)').remove();
-            
+
             smartSearch.$form.find('input:not(:checkbox), select').val('');
             smartSearch.$form.find('input:checkbox').removeAttr('checked');
-            
+
             smartSearch.settings.onClear();
-            
+
             return false;
         }
-        
+
         /* Submits form */
         smartSearch.submit = function() {
             smartSearch.$form.submit();
-            
+
             return false;
         }
-        
+
         /* Creates the searchbar */
         smartSearch.createSearchbar = function() {
             smartSearch.$searchbar = $('<ul></ul>').addClass('searchbar').append(/* Search */
@@ -72,14 +87,14 @@
             ).bind('click', function() {
                 smartSearch.$search.focus();
             });
-            
+
             smartSearch.$form.prepend(smartSearch.$searchbar);
         }
-        
+
         /* Creates the filters, adds active filters to the searchbar */
         smartSearch.createFilters = function() {
             var toClick = [];
-            
+
             var $filters = $('<div></div>').addClass('filters').append(
                 $("<hr />")
             );
@@ -91,12 +106,12 @@
                 var $filter = $('<div></div>').addClass('filter').append(
                     $('<h3></h3>').html($label.html())
                 );
-                
+
                 var $ul = $('<ul></ul>');
 
                 $.each($select.find("option"), function() {
                     var $option = $(this);
-                    
+
                     if ($option.attr('value') != '') {
                         var $li = $('<li></li>');
                         var $a = $('<a></a>').attr({
@@ -104,17 +119,17 @@
                             'data-input-id': $select.attr('id'),
                             'data-input-value': $option.attr('value')
                         }).html($option.html()).bind('click', smartSearch.applyFilter);
-                        
+
                         if ($select.attr('multiple') == 'multiple') {
                             var $checkI = $('<i></i>');
-                            
+
                             if ($option.attr('selected') == 'selected') {
                                 $checkI.addClass('icon-check');
                             }
                             else {
                                 $checkI.addClass('icon-check-empty');
                             }
-                        
+
                             $a.prepend($checkI);
                         }
 
@@ -127,17 +142,17 @@
                         $ul.append($li);
                     }
                 });
-                
+
                 $filter.append($ul);
-                
+
                 $select.hide();
                 $label.hide();
-                
+
                 $filters.append($filter);
             });
-            
+
             $filters.append($("<hr />"));
-            
+
             $.each(smartSearch.$form.find("input:visible:not(." + 'search' + ")"), function() {
                 var $field = $(this);
                 var $label = smartSearch.$form.find('label[for=' + $field.attr('id') + ']');
@@ -147,11 +162,11 @@
                 ).append(
                     $field
                 );
-                    
+
                 $filters.append($filter);
-                
+
                 $label.remove();
-                                
+
                 var $activeFilter = $('<li></li>').attr({
                     'data-input-id': $field.attr('id')
                 }).append(
@@ -161,44 +176,44 @@
                         $('<i></i>').addClass('icon-remove icon-white')
                     )
                 );
-                
+
                 if ($field.val() != '' && $field.is(':not(:checkbox)')) {
                     $activeFilter.addClass('input');
                     $activeFilter.prepend($label.html() + ': ' + $field.val());
-            
+
                     smartSearch.$searchbar.prepend($activeFilter);
                 }
                 else if ($field.is(':checkbox:checked')) {
                     $activeFilter.addClass('checkbox');
                     $activeFilter.prepend($label.html());
-            
+
                     smartSearch.$searchbar.prepend($activeFilter);
                 }
             });
-            
+
             smartSearch.$form.find("[type=submit]").hide();
-            
+
             $filters.append('<div style="clear: both;"></div>');
-            
+
             smartSearch.$filters = $filters;
             smartSearch.$form.append($filters);
-            
+
             $.each(toClick, function() {
                 $(this).click();
             });
         }
-        
+
         smartSearch.applyFilter = function() {
             var $a = $(this);
-            
+
             var $filter = $a.parents('ul:first');
             var $field = $('#' + $a.data('input-id'));
             var $label = $('label[for=' + $a.data('input-id') + ']');
-            
+
             if (smartSearch.initialized == true) {
                 if ($field.attr('multiple') == 'multiple') {
                     var $option = $field.find('option[value="' + $a.data('input-value') + '"]');
-                    
+
                     if ($option.attr('selected') == 'selected') {
                         $option.attr('selected', false);
                         $a.removeClass('active');
@@ -213,19 +228,19 @@
                 else {
                     $filter.find('a').removeClass('active');
                     $a.addClass('active');
-                    
+
                     $field.val($a.data('input-value'));
                 }
-                
+
                 $('#' + $a.data('input-id')).change();
             }
-                        
+
             var $existing = smartSearch.$searchbar.find('li[data-input-id=' + $a.data('input-id') + ']');
 
             if ($existing.length > 0) {
                 $existing.remove();
             }
-            
+
             var $activeFilter = $('<li></li>').attr({
                 'data-input-id': $a.data('input-id')
             }).append(
@@ -236,17 +251,17 @@
                 )
             );
             var selectedCount = 0;
-            
+
             $.each($filter.find('li a'), function() {
                 var $a = $(this);
-            
+
                 var $option = $field.find('option[value="' + $a.data('input-value') + '"]');
-                
+
                 if ($option.attr('selected') == 'selected') {
                     selectedCount++;
                 }
             });
-            
+
             if (selectedCount > 0) {
                 if (selectedCount <= smartSearch.settings.maxShownOptions) {
                     $.each($filter.find('li a'), function() {
@@ -265,42 +280,42 @@
 
                 smartSearch.$searchbar.prepend($activeFilter);
             }
-            
+
             $a.blur();
-            
+
             return false;
         }
-        
+
         smartSearch.removeFilter = function() {
             var $a = $(this);
             var $activeFilter = $a.parent('li');
-            
+
             if ($('#' + $activeFilter.data('input-id')).is(':checkbox')) {
                 $('#' + $activeFilter.data('input-id')).removeAttr('checked');
             }
             else {
                 $('#' + $activeFilter.data('input-id')).val('');
             }
-            
+
             if (smartSearch.initialized == true) {
                 $('#' + $activeFilter.data('input-id')).change();
             }
-            
+
             $activeFilter.remove();
-            
-            $.each(smartSearch.$filters.find('a[data-input-id=' + $activeFilter.data('input-id') + ']'), function(){  
+
+            $.each(smartSearch.$filters.find('a[data-input-id=' + $activeFilter.data('input-id') + ']'), function(){
                 var $a = $(this);
-                
+
                 $a.removeClass('active');
                 $a.find('i').removeClass('icon-check').addClass('icon-check-empty');
             });
-            
+
             return false;
         }
 
         init();
     }
-    
+
     $.fn.smartSearch = function(options) {
         return this.each(function() {
             if (undefined == $(this).data('smartSearch')) {
